a39cb00a1221c68749267d21486bb7fa04714a4a
stephanoumenos <s_dechecchi@hotmail.com>
2017-09-29 21:22:25 -0300

    Parece que funcionou

--- src/main.c
+++ src/main.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <inttypes.h>
 
-int main() {
-  int a[5];
-  int n;
+int main()
+{
+    unsigned int a[5], n, i;
+    uint8_t executado;
 
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
+    scanf("%d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &n);
 
-  for (int i=0; i<n; i++) {
+    for (i=1; i<=n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
-  }
+        executado=0;
+        if(i%a[0]==0){
+            printf("0");
+            executado=1;
+        }
+        if(i%a[1]==0){
+            printf("1");
+            executado=1;
+        }
+        if(i%a[2]==0){
+            printf("2");
+            executado=1;
+        }
+        if(i%a[3]==0){
+            printf("3");
+            executado=1;
+        }
+        if(i%a[4]==0){
+            printf("4");
+            executado=1;
+        }
+        if(!executado)
+            printf("-");
+        printf("\n");
+    }
 
-  return 0;
+    return 0;
 }
