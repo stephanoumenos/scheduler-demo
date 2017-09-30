bef0f2e7339b16dad3bf2340b8f146f362f05b7a
stephanoumenos <s_dechecchi@hotmail.com>
2017-09-29 21:24:28 -0300

    Consertei referencia pro unsigned int

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main()
     unsigned int a[5], n, i;
     uint8_t executado;
 
-    scanf("%d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &n);
+    scanf("%u %u %u %u %u %u", &a[0], &a[1], &a[2], &a[3], &a[4], &n);
 
     for (i=1; i<=n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
